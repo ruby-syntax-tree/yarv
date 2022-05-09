35a36fea4f4022b1c3565a6ed47be8e3ee916041
Kevin Newton <kddnewton@gmail.com>
2022-05-09 10:26:38 -0400

    Implement opt_str_uminus

---
+++ lib/yarv/opt_str_uminus.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module YARV
+  # ### Summary
+  #
+  # `opt_str_uminus` pushes a frozen known string value with no interpolation
+  # onto the stack.
+  #
+  # ### TracePoint
+  #
+  # `opt_str_uminus` does not dispatch any events.
+  #
+  # ### Usage
+  #
+  # ~~~ruby
+  # -"string"
+  #
+  # # == disasm: #<ISeq:<compiled>@<compiled>:1 (1,0)-(1,6)> (catch: FALSE)
+  # # 0000 opt_str_uminus                         "string", <calldata!mid:-@, argc:0, ARGS_SIMPLE>(   1)[Li]
+  # # 0003 leave 
+  # ~~~
+  #
+  class OptStrUMinus
+    attr_reader :value
+
+    def initialize(value)
+      @value = value
+    end
+
+    def execute(stack)
+      stack.push(-value)
+    end
+
+    def pretty_print(q)
+      q.text("opt_str_uminus #{value.inspect} <calldata!mid:-@, argc:0, ARGS_SIMPLE>")
+    end
+  end
+end
