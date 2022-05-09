c4b44a54313af3058069722c7bd07522a3e1979a
Adrianna Chang <adrianna.chang@shopify.com>
2022-05-09 16:32:04 -0400

    Allow compile options to be specified in tests

--- test/test_case.rb
+++ test/test_case.rb
@@ -8,16 +8,16 @@ module YARV
   class TestCase < Test::Unit::TestCase
     private
 
-    def assert_insns(expected, code)
-      iseq = YARV.compile(code)
+    def assert_insns(expected, code, **options)
+      iseq = YARV.compile(code, **options)
       assert_equal(expected, iseq.insns.map(&:class))
     end
 
-    def assert_stdout(expected, code)
+    def assert_stdout(expected, code, **options)
       original = $stdout
       $stdout = StringIO.new
 
-      YARV.compile(code).eval
+      YARV.compile(code, **options).eval
       assert_equal(expected, $stdout.string)
     ensure
       $stdout = original
