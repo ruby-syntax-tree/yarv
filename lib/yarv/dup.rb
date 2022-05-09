89b0d2541a20c84ea35d8e6534ee1bb34756e77b
Kevin Newton <kddnewton@gmail.com>
2022-05-09 11:16:54 -0400

    Set global variables

---
+++ lib/yarv/dup.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module YARV
+  # ### Summary
+  #
+  # `dup` copies the top value of the stack and pushes it onto the stack.
+  #
+  # ### TracePoint
+  #
+  # `dup` does not dispatch any events.
+  #
+  # ### Usage
+  #
+  # ~~~ruby
+  # $global = 5
+  #
+  # # == disasm: #<ISeq:<main>@-e:1 (1,0)-(1,11)> (catch: FALSE)
+  # # 0000 putobject                              5                         (   1)[Li]
+  # # 0002 dup
+  # # 0003 setglobal                              :$global
+  # # 0005 leave
+  # ~~~
+  #
+  class Dup
+    def execute(context)
+      value = context.stack.pop
+      context.stack.push(value)
+      context.stack.push(value)
+    end
+
+    def pretty_print(q)
+      q.text("dup")
+    end
+  end
+end
---
+++ lib/yarv/setglobal.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module YARV
+  # ### Summary
+  #
+  # `setglobal` sets the value of a global variable.
+  #
+  # ### TracePoint
+  #
+  # `setglobal` does not dispatch any events.
+  #
+  # ### Usage
+  #
+  # ~~~ruby
+  # $global = 5
+  #
+  # # == disasm: #<ISeq:<main>@-e:1 (1,0)-(1,11)> (catch: FALSE)
+  # # 0000 putobject                              5                         (   1)[Li]
+  # # 0002 dup
+  # # 0003 setglobal                              :$global
+  # # 0005 leave
+  # ~~~
+  #
+  class SetGlobal
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+    end
+
+    def execute(context)
+      # If we're not currently tracking the global variable, then we're going to
+      # steal the definition of it from the parent process by eval-ing it.
+      if !context.globals.key?(name) && global_variables.include?(name)
+        context.globals[name] = eval(name.to_s)
+      end
+
+      context.globals[name] = context.stack.pop
+    end
+
+    def pretty_print(q)
+      q.text("setglobal #{name.inspect}")
+    end
+  end
+end
