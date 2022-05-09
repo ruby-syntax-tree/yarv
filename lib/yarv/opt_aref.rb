675b4d0bc4a7abd7a0f710147cb3f192ef796bad
Hartley McGuire <hartley.mcguire@shopify.com>
2022-05-09 13:05:23 -0400

    Implement opt_aref
    Also delete a swap file that snuck into my last pr

---
+++ lib/yarv/opt_aref.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module YARV
+  # ### Summary
+  #
+  # `opt_aref` is a specialization of the `opt_send_without_block` instruction
+  # that occurs when the `[]` operator is used. In CRuby, there are fast paths
+  # if the receiver is an integer, array, or hash.
+  #
+  # ### TracePoint
+  #
+  # `opt_aref` can dispatch both the `c_call` and `c_return` events.
+  #
+  # ### Usage
+  #
+  # ~~~ruby
+  # 7[2]
+  #
+  # # == disasm: #<ISeq:<compiled>@<compiled>:1 (1,0)-(1,5)> (catch: FALSE)                                      │~
+  # # 0000 putobject                              7                         (   1)[Li]                           │~
+  # # 0002 putobject                              2                                                              │~
+  # # 0004 opt_aref                               <calldata!mid:[], argc:1, ARGS_SIMPLE>[CcCr]                    │~
+  # # 0006 leave
+  #
+  # ~~~
+  #
+  class OptAref
+    def execute(context)
+      left, right = context.stack.pop(2)
+      context.stack.push(left[right])
+    end
+
+    def pretty_print(q)
+      q.text("opt_aref <calldata!mid:[], argc:1, ARGS_SIMPLE>")
+    end
+  end
+end
