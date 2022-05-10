3797f13c5c5fc93d4cd0483562c608359f67af36
Étienne Barrié <etienne.barrie@gmail.com>
2022-05-10 16:22:40 +0200

    Fix case Newarray => NewArray

--- test/newarray_test.rb
+++ test/newarray_test.rb
@@ -3,9 +3,9 @@
 require_relative "./test_case"
 
 module YARV
-  class NewarrayTest < TestCase
+  class NewArrayTest < TestCase
     def test_execute
-      assert_insns([Newarray, Leave], "[]")
+      assert_insns([NewArray, Leave], "[]")
       assert_stdout(%([1, true, "string"]\n), "p([1, true, 'string'])")
     end
   end
