7b18c01e4ecfe696b63848442c312c6fcdd33ea1
Tom Winter <tom.winter@shopify.com>
2022-05-11 15:47:20 +0100

    Implement concatstrings, anytostring and objtostring
    Co-authored-by: Matt Valentine-House <matt.valentinehouse@shopify.com>

---
+++ lib/yarv/objtostring.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module YARV
+  # ### Summary
+  #
+  # `objtostring` pops a value from the stack, calls `to_s` on that value and then pushes
+  # the result back to the stack.
+  #
+  # It has fast paths for String, Symbol, Module, Class, Nil, True, False & Number.
+  # For everything else it calls `to_s`
+  #
+  # ### TracePoint
+  #
+  # `objtostring` cannot dispatch any TracePoint events.
+  #
+  # ### Usage
+  #
+  # ~~~ruby
+  # "#{5}"
+  #
+  # # == disasm: #<ISeq:<main>@-e:1 (1,0)-(1,6)> (catch: FALSE)
+  # # 0000 putobject                              ""                        (   1)[Li]
+  # # 0002 putobject                              5
+  # # 0004 dup
+  # # 0005 objtostring                            <calldata!mid:to_s, argc:0, FCALL|ARGS_SIMPLE>
+  # # 0007 anytostring
+  # # 0008 concatstrings                          2
+  # # 0010 leave
+  # ~~~
+  #
+  class ObjToString
+    attr_reader :call_data
+
+    def initialize(call_data)
+      @call_data = call_data
+    end
+
+    def call(context)
+      obj = context.stack.pop
+      result = context.call_method(call_data, obj, [])
+
+      context.stack.push(result)
+    end
+
+    def to_s
+      "%-38s %s" % ["objtostring", call_data]
+    end
+  end
+end
