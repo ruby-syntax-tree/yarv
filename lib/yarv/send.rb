2d58652bd30dc8e6aa7050ef65550acf103fcfc1
Nicholas Koh <nicholas.koh@shopify.com>
2022-05-11 15:07:43 +0800

    Add some docs

--- lib/yarv/send.rb
+++ lib/yarv/send.rb
@@ -3,18 +3,30 @@
 module YARV
   # ### Summary
   #
-  # `send`
+  # `send` invokes a method with a block.
   #
   # ### TracePoint
   #
-  # `send`
+  # `send` does not dispatch any events.
   #
   # ### Usage
   #
   # ~~~ruby
+  # "hello".tap { |i| p i }
   #
-  #
-  # ~~~
+  # # == disasm: #<ISeq:<main>@-e:1 (1,0)-(1,23)> (catch: FALSE)
+  # # 0000 putstring                              "hello"                   (   1)[Li]
+  # # 0002 send                                   <calldata!mid:tap, argc:0>, block in <main>
+  # # 0005 leave
+  # #
+  # # == disasm: #<ISeq:block in <main>@-e:1 (1,12)-(1,23)> (catch: FALSE)
+  # # local table (size: 1, argc: 1 [opts: 0, rest: -1, post: 0, block: -1, kw: -1@-1, kwrest: -1])
+  # # [ 1] i@0<Arg>
+  # # 0000 putself                                                          (   1)[LiBc]
+  # # 0001 getlocal_WC_0                          i@0
+  # # 0003 opt_send_without_block                 <calldata!mid:p, argc:1, FCALL|ARGS_SIMPLE>
+  # # 0005 leave                                  [Br]
+  # # ~~~
   #
   class Send
     attr_reader :call_data, :block_iseq
@@ -34,10 +46,8 @@ module YARV
       context.stack.push(result)
     end
 
-    def pretty_print(q)
-      # q.text(
-      #   "opt_send_without_block <calldata!mid:#{mid}, argc:#{argc}, FCALL|ARGS_SIMPLE>"
-      # )
+    def to_s
+      "%-38s %s, block in <main>" % ["send", call_data]
     end
   end
 end
